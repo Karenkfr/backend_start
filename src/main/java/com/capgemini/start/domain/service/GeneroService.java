417f1bbe480123431aacfac37d30eced9683807f
Ribeiro <karen.ribeiro@capgemini.com>
2023-01-26 09:16:06 -0300

    alteração 26/01/2023

--- src/main/java/com/capgemini/start/domain/service/GeneroService.java
+++ src/main/java/com/capgemini/start/domain/service/GeneroService.java
@@ -1,7 +1,25 @@
 package com.capgemini.start.domain.service;
 
-import com.capgemini.start.domain.entity.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
-public class GeneroService extends AbstractService<Genero, Long> {}
+import com.capgemini.start.domain.entity.Genero;
+import com.capgemini.start.domain.repository.GeneroRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class GeneroService extends AbstractService<Genero, Long> {
+
+	private final GeneroRepository repository;
+	@Override
+	protected JpaRepository<Genero, Long> getRepository() {
+		
+		return this.repository;
+	}
+	
+	
+}
 
 
--- src/main/java/com/capgemini/start/util/LoadData.java
+++ src/main/java/com/capgemini/start/util/LoadData.java
@@ -8,24 +8,35 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.capgemini.start.domain.entity.Genero;
 import com.capgemini.start.domain.entity.Tipo;
+import com.capgemini.start.domain.repository.GeneroRepository;
 import com.capgemini.start.domain.repository.TipoRepository;
 
 @Component
 public class LoadData {
-	
+
 	@Autowired
 	private TipoRepository tipoRepository;
-	
+
 	private void carregarTipos() {
 		var filmes = new Tipo(null, "Filmes", new Date(), null);
 		var series = new Tipo(null, "Series", new Date(), null);
 		tipoRepository.saveAll(List.of(filmes, series));
 	}
-	
+	@Autowired
+	private GeneroRepository generoRepository;
+
+	private void carregaGeneros() {
+		var filmes = new Genero(null, "Filmes", new Date(), null);
+		var series = new Genero(null, "Series", new Date(), null);
+		generoRepository.saveAll(List.of(filmes, series));
+	}
+
 	@PostConstruct
 	public void carregar() {
 		carregarTipos();
+		carregaGeneros();
 	}
 
 }
